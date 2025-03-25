94fb8b8498ace4978a2c5702dced81c51f4e6980
leetroyjenkins <tascher911@gmail.com>
2025-03-25 12:32:22 -0500

    updating file path

--- job-app.sh
+++ job-app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Activate the snowcones virtual environment
-# . /home/pastrychef/PycharmProjects/snowcones/venv/bin/activate
+# . ~/PycharmProjects/snowcones/venv/bin/activate
 
 # Open the Python Script
-python3 /home/pastrychef/PycharmProjects/snowcones/app.py
+python3 ~/PycharmProjects/snowcones/app.py
 
